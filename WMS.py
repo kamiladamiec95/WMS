5ef0a18cd4ef948401e5cdc62d609f25270939e6
Kamil <kamiladamiec95@wp.pl>
2022-12-20 23:02:57 +0100

    Added import product functionality

--- WMS.py
+++ WMS.py
@@ -8,9 +8,10 @@ def User_choice(section):
     user_choice = input()
     system('clear')
 
-navigation = {"Menu":["Wydania", "Przyjecia", "Inwentaryzacja", "Wyjscie"],
+navigation = {"Menu":["Wydania", "Przyjecia", "Inwentaryzacja", "Towary", "Wyjscie"],
         "Wydania":["Utworz", "Zakoncz", "Wyjscie"],        
-        "Przyjecia":["Utworz", "Zakoncz", "Wyjscie"]
+        "Przyjecia":["Utworz", "Zakoncz", "Wyjscie"],
+        "Towary":["Wyswietl", "Dodaj", "Import CSV", "Cofnij"]
         }
 
 User_choice("Menu")
@@ -19,6 +20,25 @@ if user_choice == "1":
     User_choice("Wydania")
 elif user_choice == "2":
     User_choice("Przyjecia")
+elif user_choice == "3":
+    User_choice("Inwentaryzacja")    
+elif user_choice == "4":
+    User_choice("Towary")
+    if user_choice == "1":
+        file_read = open("towary.txt", "r")
+        print(file_read.read())
+        file_read.close()
+        input()
+    elif user_choice == "3":
+        path = input("Podaj sciezke do pliku: ")
+        file_read = open("/home/kamil/Desktop/test.txt", "r")
+        file_write = open("towary.txt", "a")
+        
+        for line in file_read:
+            file_write.write(line)
 
-
+        file_read.close()
+        file_write.close()    
+elif user_choice == "5":
+    exit()    
 
