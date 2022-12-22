d628cd5340df84b52eaa9b9ba9b2648207a6d12c
Kamil <kamiladamiec95@wp.pl>
2022-12-22 22:22:43 +0100

    Added documents class

---
+++ Document.py
@@ -0,0 +1,6 @@
+class Document:
+    def __init__(self, number, date, assigned_user, value)
+        self.number = number
+        self.date = date
+        self.assigned_user = assigned_user
+        self.value = value
