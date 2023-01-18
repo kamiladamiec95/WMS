97e548284aaa81fa6133dbab7abe8b809e7d638e
Kamil <kamiladamiec95@wp.pl>
2023-01-18 21:36:28 +0100

    added sql alchemy

--- app/main.py
+++ app/main.py
@@ -3,52 +3,100 @@ from flask_wtf import FlaskForm, Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import sqlite3
+from faker import Faker
+from flask_sqlalchemy import SQLAlchemy
 # from flask_login import UserMixin, login_user, loginManager, login_required, logout_user, current_user
 
 app = Flask(__name__)
 
+app_ctx = app.app_context()
+app_ctx.push()
+
 app.config['SECRET_KEY'] = 'secret'
 
-conn = sqlite3.connect(':memory:')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users2.db'
 
-c = conn.cursor()
+db = SQLAlchemy(app)
 
-c.execute("""
-            CREATE TABLE users 
-            (
-                login text,
-                password text
+class Users(db.Model):
+    _id = db.Column("id",db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
 
-            )
-        """)
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
-c.execute("INSERT INTO users VALUES('admin@admin.pl', 'admin')")
-c.execute('SELECT * FROM users')
-itemsU = dict(c.fetchall())
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
-# print(dict(items))
-
-conn.commit()
-conn.close()
-
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     login = StringField("Login")
     password = StringField("Password")
     submit = SubmitField("Submit")
 
+class AddUser(FlaskForm):
+    login = StringField("Login")
+    password = StringField("Password")
+    name = StringField("Name") 
+    submit = SubmitField("Add")   
+
+db.create_all()
+
+users = []
+
+# user = Users(name="test", email="test2@test.pl")
+# db.session.add(user)
+# db.session.commit()
+
+faker = Faker()
+
+user = Users(name=faker.name(),
+            email=faker.email())
+db.session.add(user)
+users.append(user.name)
+db.session.commit()
+
+user = Users(name=faker.name(),
+            email=faker.email())
+db.session.add(user)
+users.append(user.name)
+
+db.session.commit()
+
+result = user.query.all()
+for i in result:
+    print(i.__dict__)
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
     # login = 'test'
-    form = LoginForm(request.form)#request.form)
+    form = LoginForm()#request.form)
     # print(form.validate_on_submit())
     
-    if request.method == "POST" and form.validate():
+    if form.validate_on_submit():
         print('test123')
         login = form.login.data 
         # if login == 'test':
         return redirect(url_for("nav"))
     print(form.errors)        
     return render_template("home3.html", form=form)
+
+
+@app.route("/add_user", methods=['GET', 'POST'])
+def add_user():
+    # login = 'test'
+    form = AddUser()#request.form)
+    # print(form.validate_on_submit())
+    
+    if form.validate_on_submit():
+        print('test123')
+        login = form.login.data 
+        # if login == 'test':
+        flash('User added successfully')
+        flash('User added successfully2')
+    print(form.errors)        
+    return render_template("add_user.html", form=form)    
 
 # @app.route("/users")
 # def users(items):
@@ -60,13 +108,20 @@ def nav():
         # result = request.form["nm"]
     return render_template("navigationBar.html")
 
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     return render_template("500.html"), 404
 
-# @app.errorhandler(500)
-# def internal_server_error(e):
-#     return render_template("500.html"), 500    
+@app.route("/users")
+def users():
+    users = ['Adam', 'Kamil']
+    return render_template("users.html", users= users)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500    
 
 if __name__ == '__main__':
     app.run(debuge=True)
